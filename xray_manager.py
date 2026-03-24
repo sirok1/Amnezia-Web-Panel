a69847ec41ed36558f1c2b37cf0571c82c2df428
Public relations & Virtual technology <101257754+PRVTPRO@users.noreply.github.com>
2026-03-24 11:49:54 +0300

    Fix Xray Dockerfile

--- xray_manager.py
+++ xray_manager.py
@@ -78,43 +78,41 @@ class XrayManager:
 RUN apk add --no-cache curl unzip bash openssl netcat-openbsd dumb-init rng-tools xz iptables ip6tables
 RUN apk --update upgrade --no-cache
 RUN mkdir -p /opt/amnezia/xray
-RUN curl -L -H "Cache-Control: no-cache" -o /root/xray.zip "https://github.com/XTLS/Xray-core/releases/download/v1.8.4/Xray-linux-64.zip" && \
-    unzip /root/xray.zip -d /usr/bin/ && \
-    chmod a+x /usr/bin/xray && \
+RUN curl -L -H "Cache-Control: no-cache" -o /root/xray.zip "https://github.com/XTLS/Xray-core/releases/download/v1.8.4/Xray-linux-64.zip" && \\
+    unzip /root/xray.zip -d /usr/bin/ && \\
+    chmod a+x /usr/bin/xray && \\
     rm /root/xray.zip
 
 # Tune network
-RUN echo -e " \n\
-  fs.file-max = 51200 \n\
-  \n\
-  net.core.rmem_max = 67108864 \n\
-  net.core.wmem_max = 67108864 \n\
-  net.core.netdev_max_backlog = 250000 \n\
-  net.core.somaxconn = 4096 \n\
-  \n\
-  net.ipv4.tcp_syncookies = 1 \n\
-  net.ipv4.tcp_tw_reuse = 1 \n\
-  net.ipv4.tcp_tw_recycle = 0 \n\
-  net.ipv4.tcp_fin_timeout = 30 \n\
-  net.ipv4.tcp_keepalive_time = 1200 \n\
-  net.ipv4.ip_local_port_range = 10000 65000 \n\
-  net.ipv4.tcp_max_syn_backlog = 8192 \n\
-  net.ipv4.tcp_max_tw_buckets = 5000 \n\
-  net.ipv4.tcp_fastopen = 3 \n\
-  net.ipv4.tcp_mem = 25600 51200 102400 \n\
-  net.ipv4.tcp_rmem = 4096 87380 67108864 \n\
-  net.ipv4.tcp_wmem = 4096 65536 67108864 \n\
-  net.ipv4.tcp_mtu_probing = 1 \n\
-  net.ipv4.tcp_congestion_control = hybla \n\
-  " | sed -e 's/^\s\+//g' | tee -a /etc/sysctl.conf && \
-  mkdir -p /etc/security && \
-  echo -e " \n\
-  * soft nofile 51200 \n\
-  * hard nofile 51200 \n\
-  " | sed -e 's/^\s\+//g' | tee -a /etc/security/limits.conf
+RUN echo "fs.file-max = 51200" >> /etc/sysctl.conf && \\
+    echo "net.core.rmem_max = 67108864" >> /etc/sysctl.conf && \\
+    echo "net.core.wmem_max = 67108864" >> /etc/sysctl.conf && \\
+    echo "net.core.netdev_max_backlog = 250000" >> /etc/sysctl.conf && \\
+    echo "net.core.somaxconn = 4096" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_tw_recycle = 0" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_fin_timeout = 30" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_keepalive_time = 1200" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.ip_local_port_range = 10000 65000" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_max_syn_backlog = 8192" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_max_tw_buckets = 5000" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_mem = 25600 51200 102400" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_rmem = 4096 87380 67108864" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_wmem = 4096 65536 67108864" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_mtu_probing = 1" >> /etc/sysctl.conf && \\
+    echo "net.ipv4.tcp_congestion_control = hybla" >> /etc/sysctl.conf
 
-RUN echo -e "#!/bin/bash\\nsysctl -p /etc/sysctl.conf 2>/dev/null\\n/usr/bin/xray -config /opt/amnezia/xray/server.json" > /opt/amnezia/start.sh && \
+RUN mkdir -p /etc/security && \\
+    echo "* soft nofile 51200" >> /etc/security/limits.conf && \\
+    echo "* hard nofile 51200" >> /etc/security/limits.conf
+
+RUN echo '#!/bin/bash' > /opt/amnezia/start.sh && \\
+    echo 'sysctl -p /etc/sysctl.conf 2>/dev/null' >> /opt/amnezia/start.sh && \\
+    echo '/usr/bin/xray -config /opt/amnezia/xray/server.json' >> /opt/amnezia/start.sh && \\
     chmod a+x /opt/amnezia/start.sh
+
 ENTRYPOINT [ "dumb-init", "/opt/amnezia/start.sh" ]
 """
         self.ssh.run_sudo_command(f"mkdir -p {dockerfile_folder}")
